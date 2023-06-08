60ebe5760378bdedf5b7e94bd1f836d4792ad0aa
Payam Torab <ptorab@parosatech.com>
2023-06-07 21:26:34 -0700

    Initial Commit

---
+++ NWSwitch/NWSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NWSwitchApp.swift
+//  NWSwitch
+//
+//  Created by Payam Torab on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct NWSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
